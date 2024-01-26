86dade3a1433322b33f851f04ea79b4688c745de
Bohan Leng <lamberhand@gmail.com>
2024-01-26 22:41:55 +0800

    Added function of reading api key from environment variables. README.md updated accordingly.

--- README.md
+++ README.md
@@ -5,20 +5,32 @@ developed by Bohan Leng
 
 
 ## Usage: 
-Typing and saving file with '@something', the script will translate 'something' into desired language(s) and write back into file in markdown table format. 
+Typing and saving file with '@something', the script will translate 'something' into desired language(s) and write back into file in Markdown table format. 
 
 Known supported editors: 
 
 - Pycharm built-in markdown editor
 - Sublime Text
+- **Obsidian (Recommended)**
 
 
 ## To include your API keys
-Create a configuration file `api_keys.ini`:
+- Create a configuration file `api_keys.ini`:
 ```ini
 [API_KEYS]
 GOOGLE_TRANS_API = YOUR_GOOGLE_API
 DEEPL_API = YOUR_DEEPL_API
 OPENAI_API = YOUR_OPENAI_API
 ```
-Only write the API keys of the engines you intend to use. 
+- Alternatively, you can specify your api keys in the environment settings:
+Use the command `vim ~/.zshrc` (or the shell you are using), append
+```shell
+export GOOGLE_TRANS_API='YOUR_GOOGLE_API'
+export DEEPL_API='YOUR_DEEPL_API'
+export OPENAI_API_KEY='YOUR_OPENAI_API'
+```
+Then refresh your shell settings with `source ~/.zshrc` (or the shell you are using).
+
+**For both methods, you only need to include the API keys of the engines you intend to use.**
+
+
--- multi_translator.py
+++ multi_translator.py
@@ -3,23 +3,36 @@ from configparser import ConfigParser
 
 
 class MultiTranslator:
-    def __init__(self, engine, src_lang, target_langues):
+    def __init__(self, engine, src_lang, target_langues, api_config_file=None):
         self.src_lang = src_lang
         self.target_langues = target_langues
         self.engine = engine
-        config = ConfigParser()
-        config.read('api_keys.ini')
-        if self.engine == 'Google Trans':
-            api_key = config.get('API_KEYS', 'GOOGLE_TRANS_API')
-            from google_translate import GoogleTranslator
-            self.the_translator = GoogleTranslator(api_key)
-        elif self.engine == 'DeepL':
-            api_key = config.get('API_KEYS', 'DEEPL_API')
-            from deepl_translate import DeepLTranslator
-            self.the_translator = DeepLTranslator(api_key)
-        elif self.engine == 'OpenAI':
-            api_key = config.get('API_KEYS', 'DEEPL_API')    # TODO
-            pass
+        if api_config_file and os.path.exists(api_config_file):
+            config = ConfigParser()
+            config.read(api_config_file)
+            if self.engine == 'Google Trans':
+                api_key = config.get('API_KEYS', 'GOOGLE_TRANS_API')
+                from google_translate import GoogleTranslator
+                self.the_translator = GoogleTranslator(api_key)
+            elif self.engine == 'DeepL':
+                api_key = config.get('API_KEYS', 'DEEPL_API')
+                from deepl_translate import DeepLTranslator
+                self.the_translator = DeepLTranslator(api_key)
+            elif self.engine == 'OpenAI':
+                api_key = config.get('API_KEYS', 'DEEPL_API')    # TODO
+                pass
+        else:
+            if self.engine == 'Google Trans':
+                api_key = os.environ.get('GOOGLE_TRANS_API')
+                from google_translate import GoogleTranslator
+                self.the_translator = GoogleTranslator(api_key)
+            elif self.engine == 'DeepL':
+                api_key = os.environ.get('DEEPL_API')
+                from deepl_translate import DeepLTranslator
+                self.the_translator = DeepLTranslator(api_key)
+            elif self.engine == 'OpenAI':
+                api_key = os.environ.get('DEEPL_API')    # TODO
+                pass
         self.translate_call = self.the_translator.translate_request
 
     def translate(self, text):
